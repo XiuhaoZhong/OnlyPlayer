6dda9bb067a4050ce7f0edf1f6a468aae9b744a5
zhongxiuhao <zhongxiuhao@qiyi.com>
2021-08-05 16:14:13 +0800

    Initial Commit

---
+++ OnlyPlayer/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  OnlyPlayer
+//
+//  Created by zhongxiuhao on 2021/8/5.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ OnlyPlayer/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  OnlyPlayer
+//
+//  Created by zhongxiuhao on 2021/8/5.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
